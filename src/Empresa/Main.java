d3a22dab1bc24aca42b21136ec496acd4d227522
Lucas <lucasafell@gmail.com>
2019-10-24 17:29:00 -0300

    método center() para centralizar a janela

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -2,6 +2,8 @@ package Empresa;
 
 import Empresa.Controller.HomeController;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +13,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         HomeController.showView();
         stage.show();
+        center();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void center() {
+        Rectangle2D monitor = Screen.getPrimary().getVisualBounds();
+        stage.setX((monitor.getWidth() - stage.getWidth()) / 2);
+        stage.setY((monitor.getHeight() - stage.getHeight()) / 2);
     }
 }
