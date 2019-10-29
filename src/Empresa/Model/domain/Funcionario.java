4c111135c26bc5933ba9341ba6a52453be94d0d6
Lucas <lucasafell@gmail.com>
2019-10-29 06:07:23 -0300

    javadocs, variaveis sobrando

--- src/Empresa/Model/domain/Funcionario.java
+++ src/Empresa/Model/domain/Funcionario.java
@@ -14,37 +14,73 @@ public class Funcionario extends Pessoa implements Serializable {
         this.cargo = cargo;
     }
 
+    /**
+     * Construtor para instâncias onde os campos não estão todos disponíveis
+     * ao mesmo tempo, utilizando os métodos sets para manipular o objeto
+     */
     public Funcionario() {
     }
 
+    /**
+     * Retorna o field
+     * @return o field
+     */
     public Empresa getEmpresa() {
         return empresa;
     }
 
+    /**
+     * Seta o field
+     * @param empresa o field
+     */
     public void setEmpresa(Empresa empresa) {
         this.empresa = empresa;
     }
 
+    /**
+     * Retorna o field
+     * @return o field
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Seta o field
+     * @param id o field
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Retorna o field
+     * @return o field
+     */
     public double getValorHora() {
         return valorHora;
     }
 
+    /**
+     * Seta o field
+     * @param valorHora o field
+     */
     public void setValorHora(double valorHora) {
         this.valorHora = valorHora;
     }
 
+    /**
+     * Retorna o field
+     * @return o field
+     */
     public Cargo getCargo() {
         return cargo;
     }
 
+    /**
+     * Seta o field
+     * @param cargo o field
+     */
     public void setCargo(Cargo cargo) {
         this.cargo = cargo;
     }
